782531995700d36bd83becd7d47330ea3001e184
Alan Fineberg <af@cs.washington.edu>
2009-12-25 03:58:23 -0800

    More tracer bullets. Some scaffolding for storing/loading game state.

--- main.py
+++ main.py
@@ -3,79 +3,86 @@ import cgi
 import os
 from django.utils import simplejson
 
+from game_state import GameState
+
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
 
-class Insect(object):
-	pass
+def load_game_by_id(gid):
+	""" TODO: use pickle to retrieve game state """
+	# if game_state.id is None:
+	# 	game_state.id = whatever
+	return GameState('foo bar', 'baz mumble')
 
-class Spider(Insect):
-	name = 'spider'
+def store_game(game_state):
+	""" TODO: pickle """
+	if game_state.id is not None:
+		# update the row
+		pass
+	else:
+		# write, get the id, then update with the id
+		pass
 
-class Beetle(Insect):
-	name = 'beetle'
-
-class Grasshopper(Insect):
-	name = 'grasshopper'
-
-class Bee(Insect):
-	name = 'bee'
-
-class Ant(Insect):
-	name = 'ant'
-
-class Hex(object):
-	def __init__(self, coord, is_playable=False, current_insect=None, is_visible=False):
-		self.coord = coord
-		self.id = '%s-%s' % self.coord
-		self.is_playable = is_playable
-		self.current_insect = current_insect
-		self.is_visible = is_visible
-
-class HiveBoard(object):
-	def __init__(self, size=7):
-		self.rows = []
-		for i in range(size):
-			row = []
-			# TODO: could make a list comp
-			for j in range(size):
-				row.append(Hex((j, i)))
-			self.rows.append(row)
-
-		# TODO math.ceil
-		self.rows[3][3] = Hex((3, 3), is_playable=True, is_visible=True)
-		self.start = self.rows[3][3]
-
-class ShowMoves(webapp.RequestHandler):
-	def get(self):
-		insect_name = self.request.get('insect_name')
-		insect_color = self.request.get('insect_color')
-		response = {'hexes': ['3-3']}
-		self.response.out.write(simplejson.dumps(response))
-		
-class Move(webapp.RequestHandler):
-	def post(self):
-		insect_name = self.request.get('insect_name')
-		insect_color = self.request.get('insect_color')
-		target_hex = self.request.get('target_hex')
-		response = {
-			'success': True,
-			'reveal_hex_ids': ['2-3', '3-4', '4-3', '3-2', '2-2', '2-4']
-		}
-		self.response.out.write(simplejson.dumps(response))
-		
 class Game(webapp.RequestHandler):
+	""" This is the main view, it sets up all the state to render the game. """
 	def get(self):
+		game_id = self.request.get('gid')
+		# TODO: validation, throw error if necessary
+		game_state = load_game_by_id(game_id)
+
 		template_values = {
-			'board': HiveBoard(),
-			# TODO: load game state
-			'pieces': (Spider(), Beetle(), Grasshopper(), Bee(), Ant()),
+			'game_state': game_state
 		}
 		path = os.path.join(os.path.dirname(__file__), 'index.html')
 		self.response.out.write(template.render(path, template_values))
+	
+	def post(self):
+		""" TODO: create a new game for a pair of users from a form, save it, and redirect. """
+		pass
+		
+class ShowMoves(webapp.RequestHandler):
+	""" Responds to xhr requests when an insect is selected, to display where it can move. """
+	def get(self):
+		game_id = self.request.get('gid')
+		game_state = load_game_by_id(game_id)
+
+		insect_name = self.request.get('insect_name')
+		insect_color = self.request.get('insect_color')
+		response = {
+			'success': True,
+			'hexes': [a_hex.id for a_hex in game_state.show_moves(insect_color, insect_name)]
+		}
+		self.response.out.write(simplejson.dumps(response))
+		
+class Move(webapp.RequestHandler):
+	""" Respond to xhr when an insect is moved to a specific hex tile. """
+	def post(self):
+		""" Submit a move and return what changed on the board, or an error. """
+		# TODO: check for end of game conditions
+		game_id = self.request.get('gid')
+		game_state = load_game_by_id(game_id)
+
+		insect_name = self.request.get('insect_name')
+		insect_color = self.request.get('insect_color')
+		target_hex = self.request.get('target_hex')
+
+		try:
+			result = game_state.move(insect_color, insect_name, target_hex)
+			response = {
+				'success': True,
+				'reveal_hex_ids': [a_hex.id for a_hex in result[0]],
+				'hide_hex_ids': [a_hex.id for a_hex in result[1]],
+			}
+		except:
+			response = {
+				'success': False,
+				'msg': 'Something went wrong, :-/'
+			}
+
+		self.response.out.write(simplejson.dumps(response))
 		
 application = webapp.WSGIApplication([
 									('/', Game),
